9aeb06129eb414a89321a1625193019a9c784dd9
Tracy Reith <treith@treith.com>
2019-01-30 20:34:41 -0800

    Add separate buttons for creating and managing bank account, attempt to record deposits and withdrawals - fail

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,42 @@
 // ** BUSINESS LOGIC
 
-function BankAccount(name, balance) {
+function BankAccount(name, balance, deposit, withdrawal) {
   this.name = name;
   this.balance = balance;
+  this.deposit = deposit;
+  this.withdrawal = withdrawal;
 }
 
-BankAccount.prototype.deposit = function(deposit) {
-  return this.balance = this.balance + deposit;
+BankAccount.prototype.deposit = function(balance, deposit) {
+  return this.balance = this.balance + this.deposit;
 }
 
-BankAccount.prototype.withdrawal = function(withdrawal) {
-  return this.balance = this.balance - withdrawal;
+BankAccount.prototype.withdrawal = function(balance, withdrawal) {
+  return this.balance = this.balance - this.withdrawal;
 }
 
 
 // ** UI LOGIC
+var newBankAccount = new BankAccount();
 
 $(document).ready(function() {
   $("#create-account").submit(function(event){
     event.preventDefault();
+    // debugger;
     var name = $("#name").val();
-    var initialDeposit =  parseFloat($("#initialDeposit").val());
+    var initialDeposit = parseFloat($("#initialDeposit").val());
+    var newBankAccount = new BankAccount(name,initialDeposit, 0, 0);
+    // var newBalance = newBankAccount.balance;
+    console.log(newBankAccount);
+    $(".account-balance").text(initialDeposit);
+  });
+
+  $("#manage-account").submit(function(event){
+    event.preventDefault();
     var depositAmount = parseFloat($("#depositAmount").val());
     var withdrawalAmount = parseFloat($("#withdrawalAmount").val());
-    var newBankAccount = new BankAccount(name,initialDeposit);
-    console.log(newBankAccount);
-    var newBalance = newBankAccount.balance;
-    $("p.account-balance").text(newBalance);
-  // });
-  //   $("#manage-account").click(function(event){
-  //     event.preventDefault();
+    var currentBalance = parseFloat($(".accountBalance").val());
+    var updateBalance = newBankAccount.deposit(currentBalance, depositAmount);
+    console.log(updateBalance);
   });
 });
