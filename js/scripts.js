fd937b552460b6993a96b8f378e3538d754fac27
Tracy Reith <treith@treith.com>
2019-01-30 23:34:15 -0800

    Add comments to code indicating where I am having trouble getting js code to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@
 function BankAccount(name, balance, deposit, withdrawal) {
   this.name = name;
   this.balance = balance;
-  this.deposit = deposit;
-  this.withdrawal = withdrawal;
+  this.deposit = deposit; //** I don't think these really need to be properties of a bank account object
+  this.withdrawal = withdrawal; //** I don't think these really need to be properties of a bank account object
 }
 
 BankAccount.prototype.deposit = function(balance, deposit) {
@@ -17,7 +17,7 @@ BankAccount.prototype.withdrawal = function(balance, withdrawal) {
 
 
 // ** UI LOGIC
-var newBankAccount = new BankAccount();
+var newBankAccount = new BankAccount(name); //** Why does this work when I put name as the single parameter in the parens and no other parameter?
 
 $(document).ready(function() {
   $("#create-account").submit(function(event){
@@ -35,8 +35,9 @@ $(document).ready(function() {
     event.preventDefault();
     var depositAmount = parseFloat($("#depositAmount").val());
     var withdrawalAmount = parseFloat($("#withdrawalAmount").val());
-    var currentBalance = parseFloat($(".accountBalance").val());
-    var updateBalance = newBankAccount.deposit(currentBalance, depositAmount);
+    var currentBalance = parseFloat($(".accountBalance").val()); //** I don't think this would do anything
+    // var currentBalance = newBankAccount.this.balance; //** Cannot read property of balance - how do I get this.balance? What good is it as a property of the object if I can't get it?
+    var updateBalance = newBankAccount.deposit(currentBalance, depositAmount); //**newBankAccount.deposit is not a function. How am I supposed to use prototypes?
     console.log(updateBalance);
   });
 });
