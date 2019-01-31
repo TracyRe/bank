45fe71b84b58fdb16222ce4dac836ad0a09da9c2
Student <example@example.com>
2019-01-31 13:03:16 -0800

    Add fixed code help from Dino N

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // ** BUSINESS LOGIC
 
-function BankAccount(name, balance) {
+function BankAccount() {
   this.name = name;
-  this.balance = balance;
+  this.balance = 0;
   // this.deposit = deposit; //** I don't think these really need to be properties of a bank account object
   // this.withdrawal = withdrawal; //** I don't think these really need to be properties of a bank account object
   // ** when I comment these out, I get undefined function for the deposit prototype when I console log it, which is better than eror: newBankAccount.deposit is not a function when these were part of the constructor
@@ -18,16 +18,18 @@ BankAccount.prototype.withdrawal = function(balance, withdrawal) {
 
 
 // ** UI LOGIC
-var newBankAccount = new BankAccount(name); //** Why does this work when I put name as the single parameter in the parens and no other parameter?
+// var newBankAccount = new BankAccount(name); //** Why does this work when I put name as the single parameter in the parens and no other parameter?
 
 $(document).ready(function() {
+  var newBankAccount = new BankAccount();
+
   $("#create-account").submit(function(event){
     event.preventDefault();
     // debugger;
     var name = $("#name").val();
     var initialDeposit = parseFloat($("#initialDeposit").val());
-    var newBankAccount = new BankAccount(name,initialDeposit);
-    // var newBalance = newBankAccount.balance;
+    newBankAccount.name = name;
+    newBankAccount.balance = initialDeposit;
     console.log(newBankAccount);
     $(".account-balance").text(initialDeposit);
 
@@ -40,8 +42,8 @@ $(document).ready(function() {
     var currentBalance = parseFloat($(".account-balance").val()); //** I don't think this would do anything
     // var currentBalance = newBankAccount.this.balance; //** Cannot read property of balance - how do I get this.balance? What good is it as a property of the object if I can't get it?
     // balance = currentBalance; //** this did not help when I used it to replace 'currentBalance' as the argument in the line below
-  var updateBalance = newBankAccount.deposit(currentBalance, depositAmount); //**newBankAccount.deposit is not a function. How am I supposed to use prototypes?
-  console.log(currentBalance);
-    console.log(updateBalance);
+    console.log(newBankAccount.deposit(currentBalance, depositAmount)); //**newBankAccount.deposit is not a function. How am I supposed to use prototypes?
+    console.log(currentBalance);
+    console.log(newBankAccount.balance);
   });
 });
